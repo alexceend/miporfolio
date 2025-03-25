7717a08aa632768d93dfaaab998ec060895be843
alexceend <alexceend@gmail.com>
2025-03-25 15:51:48 +0100

    Deployment files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alexceend.github.io',
+  base: 'miporfolio',
+
+
   vite: {
     plugins: [tailwindcss()],
   },
