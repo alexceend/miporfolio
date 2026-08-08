ce442352c52343324ecff9750d1dfeadabaff589
alexceend <alexceend@gmail.com>
2026-08-08 18:25:05 +0200

    Move projects to content.js in order to delete repetitions/duplicity

---
+++ src/data/content.js
@@ -0,0 +1,112 @@
+export const projects = [
+  {
+    title: 'Catch\'em all. Fishingrod',
+    image_paths: ['/miporfolio/catchemall-1.webp', '/miporfolio/catchemall-2.webp', '/miporfolio/catchemall-3.webp'],
+    description: 'Un juego simple en unity sobre pesca para experimentar con motores de creación de videojuegos',
+    year: '2025',
+    link: 'https://github.com/alexceend/UnityFishingGame'
+  },
+  {
+    title: 'NVIM PLUGINS',
+    image_paths: ['/miporfolio/minevim.webp'],
+    description: 'Plugins para Nvim hechos en lua para customizar mi Arch - Linux',
+    year: '2025',
+    link: 'https://github.com/alexceend/nvim-plugins'
+  },
+  {
+    title: "Danny's frog",
+    image_paths: ['/miporfolio/danny-1.webp', '/miporfolio/danny-2.webp'],
+    description: 'Videojuego hecho en java para experimentar con Swing',
+    year: '2024',
+    link: 'https://github.com/alexceend/Danny-s-frog'
+  },
+  {
+    title: 'Jojo bird',
+    image_paths: ['/miporfolio/jojobird-1.webp', '/miporfolio/jojobird-2.webp'],
+    description: 'Videojuego hecho en java junto a 2 compañeros para una Game JAM de la universidad.',
+    year: '2024',
+    link: 'https://github.com/alexceend/jojo-bird'
+  },
+  {
+    title: 'Dana database',
+    image_paths: ['/miporfolio/dana-1.webp', '/miporfolio/dana-2.webp'],
+    description: 'Base de datos SQL simple que contiene información relevante sobre la DANA of Valencia. Incluyendo puntos de entrega de ayudas, páginas web de información y ayuda y las poblaciones y ciudades afectadas',
+    year: '2025',
+    link: 'https://github.com/alexceend/dana-data-base'
+  },
+  {
+    title: 'Solomillo Mus Counter',
+    image_paths: ['/miporfolio/solomo-1.jpg'],
+    description: "Aplicación Android Kotlin para contar los puntos del juego de cartas 'Mus'",
+    year: '2025',
+    link: 'https://github.com/alexceend/solomillo-mus-counter'
+  },
+  {
+    title: 'Unfollowers Insta',
+    image_paths: ['/miporfolio/unfollowers-1.webp', '/miporfolio/unfollowers-2.webp'],
+    description: 'Script en python que te permite ver los usuarios que no te siguen de vuelta en Instagram.',
+    year: '2024',
+    link: 'https://github.com/alexceend/see-who-unfollowed-you-insta'
+  },
+  {
+    title: 'Esteganografía Java',
+    image_paths: ['/miporfolio/esteganografia-1.webp'],
+    description: 'Programa en java que te permite ocultar texto en imágenes utilizando esteganografía.',
+    year: '2023',
+    link: 'https://github.com/alexceend/EsteganografiaJava'
+  },
+  {
+    title: 'Cats and spikes',
+    image_paths: ['/miporfolio/cats-spikes-1.webp', '/miporfolio/cats-spikes-2.webp'],
+    description: 'Mi primer videojuego hecho en Godot.',
+    year: '2019',
+    link: 'https://github.com/alexceend/Cats-Spikes'
+  },
+  {
+    title: 'Upcoming projects',
+    image_paths: ['/miporfolio/question-mark.webp'],
+    description: 'Upcoming projects...',
+    year: '',
+    link: 'https://github.com/alexceend'
+  }
+];
+
+export const contributions = [
+  {
+    title: 'La velada del año - Midudev',
+    image_paths: ['/miporfolio/web-velada.webp'],
+    description: "Web de 'La velada del año' creada por Midudev",
+    year: '',
+    link: 'https://github.com/midudev/la-velada-web-oficial',
+    prs: [
+      { name: 'midudev/la-velada-web-oficial#1483 ', link: 'https://github.com/midudev/la-velada-web-oficial/pull/1483' },
+      { name: 'midudev/la-velada-web-oficial#1215 ', link: 'https://github.com/midudev/la-velada-web-oficial/pull/1215' },
+      { name: 'midudev/la-velada-web-oficial#1205 ', link: 'https://github.com/midudev/la-velada-web-oficial/pull/1205' }
+    ]
+  },
+  {
+    title: 'GrapheneOS - Camera',
+    image_paths: ['/miporfolio/graphene-os.webp'],
+    description: 'Sistema operativo de código abierto centrado en la privacidad y seguridad para dispositivos móviles',
+    year: '',
+    link: 'https://github.com/GrapheneOS/GrapheneOS',
+    prs: [
+      { name: 'GrapheneOS/GrapheneOS#684 ', link: 'https://github.com/GrapheneOS/Camera/pull/684' }
+    ]
+  },
+  {
+    title: 'SharpEmu',
+    image_paths: ['/miporfolio/sharpemu.webp'],
+    description: 'Emulador experimental de PS5 para Windows, Linux y macOS ',
+    year: '',
+    link: 'https://github.com/SharpEmu/SharpEmu',
+    prs: [
+      { name: 'sharpemu/sharpemu#161 ', link: 'https://github.com/SharpEmu/SharpEmu/pull/161' },
+      { name: 'sharpemu/sharpemu#162 ', link: 'https://github.com/SharpEmu/SharpEmu/pull/162' },
+      { name: 'sharpemu/sharpemu#165 ', link: 'https://github.com/SharpEmu/SharpEmu/pull/165' },
+      { name: 'sharpemu/sharpemu#148 ', link: 'https://github.com/SharpEmu/SharpEmu/pull/148' },
+      { name: 'sharpemu/sharpemu#279 ', link: 'https://github.com/SharpEmu/SharpEmu/pull/279' },
+      { name: 'sharpemu/sharpemu#735 ', link: 'https://github.com/SharpEmu/SharpEmu/pull/735' }
+    ]
+  }
+];
